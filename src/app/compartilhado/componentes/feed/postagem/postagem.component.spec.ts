2c588379acdae0c60fc37df4ae760a509649468e
Cezar Juventino Martins Neto <103068760+CezarMNeto@users.noreply.github.com>
2022-06-21 16:06:53 -0300

    correção erro core

--- src/app/compartilhado/componentes/feed/postagem/postagem.component.spec.ts
+++ src/app/compartilhado/componentes/feed/postagem/postagem.component.spec.ts
@@ -3,23 +3,22 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PostagemComponent } from './postagem.component';
 
 describe('PostagemComponent', () => {
-    let component: PostagemComponent;
-    let fixture: ComponentFixture<PostagemComponent>;
+  let component: PostagemComponent;
+  let fixture: ComponentFixture<PostagemComponent>;
 
-    beforeEach(async () => {
-        await TestBed.configureTestingModule({
-            declarations: [PostagemComponent]
-        })
-            .compileComponents();
-    });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PostagemComponent],
+    }).compileComponents();
+  });
 
-    beforeEach(() => {
-        fixture = TestBed.createComponent(PostagemComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
-    });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PostagemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-    it('should create', () => {
-        expect(component).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
